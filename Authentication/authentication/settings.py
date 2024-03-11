e0f9f8a93135763e0e1c8a782aa9da4b0e05a30d
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-12 02:43:03 +0330

    feat: update nginx config

--- Authentication/authentication/settings.py
+++ Authentication/authentication/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#!)@4ew623@&!!rs&$ne8f^i4_4s4=p)p+tj@7u+uyrvj7519w
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+ALLOWED_HOSTS = '*'
 
 AUTH_USER_MODEL = 'api.User'
 
@@ -177,4 +177,3 @@ REST_FRAMEWORK = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'profile_pictures/')
 MEDIA_URL = '/profile_pictures/'
-ALLOWED_HOSTS = ['*']
