86472290d29c7a9faec4618f06b2bafe3a9c448a
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-11 23:32:10 +0330

    feat: update permission classes

--- Authentication/api/sender.py
+++ Authentication/api/sender.py
@@ -3,19 +3,17 @@ import json
 mq= 'amqp://guest:guest@rabbitmq:5672'
 
 def send_otp(otp):
-    print("otp password")
-    print(otp.password)
-    # try:
-    #     api = KavenegarAPI("444E6C6378534967385758394B41572B57434F7A4C644D664558485631704464")
-    #     params = {
-    #         'sender': '0018018949161',
-    #         'receptor': otp.receiver,
-    #          'template': 'verify',
-    #         'token': otp.password,
-    #         'type': 'sms'
-    #     }   
-    #     response = api.verify_lookup(params)
-    # except APIException as e: 
-    #     print (str(e))
-    # except HTTPException as e: 
-    #     print (str(e))
+    try:
+        api = KavenegarAPI("444E6C6378534967385758394B41572B57434F7A4C644D664558485631704464")
+        params = {
+            'sender': '0018018949161',
+            'receptor': otp.receiver,
+             'template': 'verify',
+            'token': otp.password,
+            'type': 'sms'
+        }   
+        response = api.verify_lookup(params)
+    except APIException as e: 
+        print (str(e))
+    except HTTPException as e: 
+        print (str(e))
