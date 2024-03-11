d40ece3b7889a8b3f093f32ed02e76ad3be2eaec
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-11 21:12:29 +0330

    feat: update rabbitmq port

--- BaseInfo/api/management/commands/pubSub.py
+++ BaseInfo/api/management/commands/pubSub.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Code for your custom command goes here
         connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='0.0.0.0',credentials=pika.PlainCredentials(username='rabbitmq',password='rabbitmq')))
+        pika.ConnectionParameters(host='rabbitmq',credentials=pika.PlainCredentials(username='rabbitmq',password='rabbitmq')))
 
         channel = connection.channel(1)
 
