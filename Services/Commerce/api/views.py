007f40eeebc020112fea6549002a3887f97ecf3f
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-17 00:10:23 +0330

    feat: log:

--- Services/Commerce/api/views.py
+++ Services/Commerce/api/views.py
@@ -157,6 +157,7 @@ class AddCommerceView(generics.CreateAPIView):
         user,_ = request.user
         user = json.loads(user)
         temp = copy.deepcopy(request.data)
+        print(request.data["images"])
         images = request.FILES.getlist('images')
         print(images)
         temp["user_id"] = user["id"]
