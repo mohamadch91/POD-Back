3bd4c73b5d421dab9bc193d8ac5b48c35ec5fa18
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-03-16 12:45:01 +0330

    feat: change acks method

--- Services/Commerce/api/views.py
+++ Services/Commerce/api/views.py
@@ -20,7 +20,7 @@ import json
  
 
 class CommerceListView(generics.ListAPIView):
-    permission_classes = [IsAuthenticatedM]
+    # permission_classes = [IsAuthenticatedM]
     queryset =Commerce.objects.all()
     def get(self, request):
         commerce = Commerce.objects.all()
@@ -97,7 +97,7 @@ class CommerceListView(generics.ListAPIView):
         return Response(final_response,status=status.HTTP_200_OK)
     
 class CommerceDetailView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticatedM]
+    # permission_classes = [IsAuthenticatedM]
 
     queryset =Commerce.objects.all()
     def get(self, request):
