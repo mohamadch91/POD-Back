02829fefa83f571627a022042b79b9792f5a9a53
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-17 11:15:16 +0330

    feat: add base info API's

--- BaseInfo/api/urls.py
+++ BaseInfo/api/urls.py
@@ -17,6 +17,19 @@ urlpatterns = [
     path('serviceStatus/', ServiceStatusView.as_view(), name='get all cities'),
     path('activityTypes/', ActivityTypeView.as_view(), name='get all cities'),
     path('businessVariety/', BusinessTypeView.as_view(), name='get all cities'),
+    # admin panel API's
+    path('commerceBrandsAdmin/', CommerceBrandsAdminView.as_view(), name='get all cities'),
+    path('commerCategoriesAdmin/', CommerceCategoryAdminView.as_view(), name='get all cities'),
+    path('serviceBrandsAdmin/', ServiceBrandsAdminView.as_view(), name='get all cities'),
+    path('serviceCategoriesAdmin/', ServiceCategoryAdminView.as_view(), name='get all cities'),
+    path('saleMethodsAdmin/', SaleMethodsAdminView.as_view(), name='get all cities'),
+    path('deliveryMethodsAdmin/', DeliveryMethodsAdminView.as_view(), name='get all cities'),
+    path('commerceStatusAdmin/', CommerceStatusAdminView.as_view(), name='get all cities'),
+    path('serviceStatusAdmin/', ServiceStatusAdminView.as_view(), name='get all cities'),
+    path('activityTypesAdmin/', ActivityTypeAdminView.as_view(), name='get all cities'),
+    path('businessVarietyAdmin/', BusinessTypeAdminView.as_view(), name='get all cities'),
+    
+
 
     path('admin/', admin.site.urls),
 
--- BaseInfo/api/views.py
+++ BaseInfo/api/views.py
@@ -121,3 +121,289 @@ class OrderStatusView(generics.ListAPIView):
         return Response(serializer.data,status=status.HTTP_200_OK)
     
 
+class CommerceBrandsAdminView(APIView):
+
+
+    def get(self, request):
+        commerceBrands = CommerceBrands.objects.all()
+        serializer = CommerceBrandsSerializer(commerceBrands,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = CommerceBrandsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request):
+        commerceBrand = get_object_or_404(CommerceBrands, id=request.data["id"])
+        serializer = CommerceBrandsSerializer(commerceBrand, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        id = request.GET.get('id')
+        commerceBrand = get_object_or_404(CommerceBrands, id = id)
+        commerceBrand.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommerceCategoryAdminView(APIView):
+
+
+    def get(self, request):
+        commerceCategories = CommerceCategory.objects.all()
+        serializer = CommerceCategorySerializer(commerceCategories,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = CommerceCategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request):
+        commerceCategory = get_object_or_404(CommerceCategory, id=request.data["id"])
+        serializer = CommerceCategorySerializer(commerceCategory, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        id = request.GET.get('id')
+        commerceCategory = get_object_or_404(CommerceCategory, id = id)
+        commerceCategory.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class ServiceBrandsAdminView(APIView):
+    
+        def get(self, request):
+            serviceBrands = ServiceBrands.objects.all()
+            serializer = ServiceBrandsSerializer(serviceBrands,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = ServiceBrandsSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            serviceBrand = get_object_or_404(ServiceBrands, id=request.data["id"])
+            serializer = ServiceBrandsSerializer(serviceBrand, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            serviceBrand = get_object_or_404(ServiceBrands, id = id)
+            serviceBrand.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ServiceCategoryAdminView(APIView):
+        
+            def get(self, request):
+                serviceCategories = ServiceCategory.objects.all()
+                serializer = ServiceCategorySerializer(serviceCategories,many=True)
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            
+            def post(self, request):
+                serializer = ServiceCategorySerializer(data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data,status=status.HTTP_201_CREATED)
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+            def put(self, request):
+                serviceCategory = get_object_or_404(ServiceCategory, id=request.data["id"])
+                serializer = ServiceCategorySerializer(serviceCategory, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data,status=status.HTTP_200_OK)
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+            def delete(self, request):
+                id = request.GET.get('id')
+                serviceCategory = get_object_or_404(ServiceCategory, id = id)
+                serviceCategory.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            
+
+class SaleMethodsAdminView(APIView):
+        
+        def get(self, request):
+            saleMethods = SaleMethod.objects.all()
+            serializer = SaleMethodSerializer(saleMethods,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = SaleMethodSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            saleMethod = get_object_or_404(SaleMethod, id=request.data["id"])
+            serializer = SaleMethodSerializer(saleMethod, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            saleMethod = get_object_or_404(SaleMethod, id = id)
+            saleMethod.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+class DeliveryMethodsAdminView(APIView):
+    
+        def get(self, request):
+            deliveryMethods = DeliveryMethod.objects.all()
+            serializer = DeliveryMethodSerializer(deliveryMethods,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = DeliveryMethodSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            deliveryMethod = get_object_or_404(DeliveryMethod, id=request.data["id"])
+            serializer = DeliveryMethodSerializer(deliveryMethod, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            deliveryMethod = get_object_or_404(DeliveryMethod, id = id)
+            deliveryMethod.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommerceStatusAdminView(APIView):
+    
+        def get(self, request):
+            commerceStatus = CommerceStatus.objects.all()
+            serializer = CommerceStatusSerializer(commerceStatus,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = CommerceStatusSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            commerceStatus = get_object_or_404(CommerceStatus, id=request.data["id"])
+            serializer = CommerceStatusSerializer(commerceStatus, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            commerceStatus = get_object_or_404(CommerceStatus, id = id)
+            commerceStatus.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class ServiceStatusAdminView(APIView):
+
+        def get(self, request):
+            serviceStatus = ServiceStatus.objects.all()
+            serializer = ServiceStatusSerializer(serviceStatus,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = ServiceStatusSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            serviceStatus = get_object_or_404(ServiceStatus, id=request.data["id"])
+            serializer = ServiceStatusSerializer(serviceStatus, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            serviceStatus = get_object_or_404(ServiceStatus, id = id)
+            serviceStatus.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ActivityTypeAdminView(APIView):
+    
+        def get(self, request):
+            activityTypes = ActivityType.objects.all()
+            serializer = ActivityTypeSerializer(activityTypes,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        def post(self, request):
+            serializer = ActivityTypeSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def put(self, request):
+            activityType = get_object_or_404(ActivityType, id=request.data["id"])
+            serializer = ActivityTypeSerializer(activityType, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        def delete(self, request):
+            id = request.GET.get('id')
+            activityType = get_object_or_404(ActivityType, id = id)
+            activityType.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BusinessTypeAdminView(APIView):
+        
+            def get(self, request):
+                businessTypes = BusinessVariety.objects.all()
+                serializer = BusinessVarietySerializer(businessTypes,many=True)
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            
+            def post(self, request):
+                serializer = BusinessVarietySerializer(data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data,status=status.HTTP_201_CREATED)
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+            def put(self, request):
+                businessType = get_object_or_404(BusinessVariety, id=request.data["id"])
+                serializer = BusinessVarietySerializer(businessType, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data,status=status.HTTP_200_OK)
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+            def delete(self, request):
+                id = request.GET.get('id')
+                businessType = get_object_or_404(BusinessVariety, id = id)
+                businessType.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            
+
