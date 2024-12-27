50a31f2fa2cb5b0b7ce68981f9cb04be6e742bff
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-27 15:50:13 +0330

    fix: remove collect statics

--- BaseInfo/rabbit.sh
+++ BaseInfo/rabbit.sh
@@ -10,12 +10,6 @@ python3 manage.py loaddata api/fixtures/service_category.json
 python3 manage.py loaddata api/fixtures/city.json
 python3 manage.py loaddata api/fixtures/sale_method.json
 python3 manage.py loaddata api/fixtures/delivery_method.json
-python3 manage.py collectstatic
-
-echo yes
-echo yes
-echo yes
-
 
 python3 manage.py pubSub > a.txt  2>&1 &
 echo -ne '\n'
