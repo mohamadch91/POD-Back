af3a05e4d210b9e23200d0d6b73077efbb9f0985
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-04-16 15:02:11 +0330

    feat: uddate news

--- content/api/views.py
+++ content/api/views.py
@@ -101,7 +101,7 @@ class NewsAdminView(APIView):
             news = news[start:end]
         
         serializer = NewsSerializer(news, many=True)
-        images = NewsImages.objects.filter(news=news.values_list("id"))
+        images = NewsImages.objects.filter(news__in=news)
         images_serializer = NewsImagesSerializer(images,many=True)
         final_response = []
         for ser in serializer.data:
