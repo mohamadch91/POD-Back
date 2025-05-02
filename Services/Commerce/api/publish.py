acf9341e03bddc335957f78cc3e0327ddc24a167
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-05-02 06:48:06 +0330

    fix: convert object to json

--- Services/Commerce/api/publish.py
+++ Services/Commerce/api/publish.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import grpc 
-from .rpc import user_pb2,user_pb2_grpc
+from .rpc import user_pb2,user_pb2_grpc,base_info_pb2_grpc,base_info_pb2
 import os
 import json
 AUTH_ADDRESS = os.environ.get('AUTH_GRPC_ADDRESS', '[::]:50051')
@@ -34,3 +34,16 @@ def get_user(id):
             print(f"Error: {e}")
             return None
 
+def get_info(data):
+    with grpc.insecure_channel(BASE_INFO_ADDRESS) as channel:
+        stub = base_info_pb2_grpc.BaseInfoControllerStub(channel)
+        request =[]
+        for i in data:
+            request.append(base_info_pb2.BaseInfoRequest(name=i,id=data[i]))
+        final_request = base_info_pb2.GetBaseInfoRequest(baseInfo=request)
+        try:
+            response = stub.GetInfo(final_request)
+            return response
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
