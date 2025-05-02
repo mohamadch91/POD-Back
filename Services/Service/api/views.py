8ca7a5a650b2f6ba7879fbbd0850a21376b145c4
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-05-02 22:38:04 +0330

    fix: negoatiate api

--- Services/Service/api/views.py
+++ Services/Service/api/views.py
@@ -251,6 +251,7 @@ class NegotiateServiceView(generics.CreateAPIView):
     queryset =Service.objects.all()
     def post(self, request):
         user = request.user
+        print(user)
         temp = copy.deepcopy(request.data)
         temp["user_id"] = user["id"]
         serializer = ServiceNegotiateSerializer(data = temp)
