22e0b208387e47177d3ced0ff21a5e71cce05dc3
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-05-03 15:25:02 +0330

    fix: admin problem

--- BaseInfo/api/permissions.py
+++ BaseInfo/api/permissions.py
@@ -18,7 +18,7 @@ class IsAuthenticated(permissions.BasePermission):
 
 
     def has_permission(self, request, view):
-        user = authenitcate(request)
+        user,_ = authenitcate(request)
         # user=None
         if( user):
             request.user = user
@@ -35,7 +35,7 @@ class IsAdminUser(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        user = authenitcate(request)
+        user,_ = authenitcate(request)
         if(user and user.is_superuser):
             request.user = user
             return True
