f7eb4437e72a5dfb3e919c4a22c3755fd6bdf588
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-05-06 09:47:30 +0330

    feat: remove fixtures

--- BaseInfo/grpc.sh
+++ BaseInfo/grpc.sh
@@ -3,13 +3,7 @@
 
 python3 manage.py makemigrations && python3 manage.py migrate
 python3 manage.py loaddata api/fixtures/province.json
-python3 manage.py loaddata api/fixtures/commerce_brand.json
-python3 manage.py loaddata api/fixtures/commerce_category.json
-python3 manage.py loaddata api/fixtures/service_brand.json
-python3 manage.py loaddata api/fixtures/service_category.json
 python3 manage.py loaddata api/fixtures/city.json
-python3 manage.py loaddata api/fixtures/sale_method.json
-python3 manage.py loaddata api/fixtures/delivery_method.json
 
 echo -ne '\n'
 echo -ne '\n'
