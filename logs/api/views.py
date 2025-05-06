979a6ca6ae44b6e252a947e749e8fa242d37721e
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-05-07 00:05:12 +0330

    feat: change logs response

--- logs/api/views.py
+++ logs/api/views.py
@@ -14,7 +14,7 @@ from django.http import QueryDict
 from django.db.models import Case, When
 from django.db.models import Sum
 import json
-
+from .customResponse import CustomResponse,CustomMessage
 
 class PermiumRequestView(APIView):
 
@@ -22,8 +22,8 @@ class PermiumRequestView(APIView):
         serializer = PermiumRequestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse(serializer.data, status=status.HTTP_201_CREATED,message=CustomMessage(type=5,data="درخواست پرمیوم"))
+        return CustomResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست پرمیوم با خطا مواجه شد"))
     
 class ConsultationRequestView(APIView):
 
@@ -32,8 +32,8 @@ class ConsultationRequestView(APIView):
         serializer = ConsultationRequestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse(serializer.data, status=status.HTTP_201_CREATED,message=CustomMessage(type=5,data="درخواست مشاوره"))
+        return CustomResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست مشاوره با خطا مواجه شد"))
 
 
 
@@ -42,26 +42,26 @@ class PermiumRequestAdminView(APIView):
     def get(self, request):
         permium_requests = PermiumRequest.objects.all()
         serializer = PermiumRequestSerializer(permium_requests, many=True)
-        return Response(serializer.data)
+        return CustomResponse(serializer.data,status=status.HTTP_200_OK,message=CustomMessage(type=1,data="درخواست پرمیوم"))
     
     def put (self,request):
         if('id' not in request.data or 'id' =='' ):
-            return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست پرمیوم"))
         id=request.data["id"]
         permium_request = get_object_or_404(PermiumRequest,id=id)
         serializer = PermiumRequestSerializer(permium_request,data=request.data,partial=True)
         if(serializer.is_valid()):
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse(serializer.data,status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=6,data="درخواست پرمیوم"))
+        return CustomResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست پرمیوم با خطا مواجه شد"))
 
     def delete(self,request):
         id = request.GET.get('id')
         if(id == None):
-            return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست پرمیوم"))
         permium_request = get_object_or_404(PermiumRequest,id=id)
         permium_request.delete()
-        return Response("deleted",status=status.HTTP_202_ACCEPTED)
+        return CustomResponse("deleted",status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=7,data="درخواست پرمیوم"))
     
 
 class ConsultationRequestAdminView(APIView):
@@ -69,26 +69,26 @@ class ConsultationRequestAdminView(APIView):
         def get(self, request):
             consultation_requests = ConsultationRequest.objects.all()
             serializer = ConsultationRequestSerializer(consultation_requests, many=True)
-            return Response(serializer.data)
+            return CustomResponse(serializer.data,status=status.HTTP_200_OK,message=CustomMessage(type=1,data="درخواست مشاوره"))
         
         def put (self,request):
             if('id' not in request.data or 'id' =='' ):
-                return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+                return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست مشاوره"))
             id=request.data["id"]
             consultation_request = get_object_or_404(ConsultationRequest,id=id)
             serializer = ConsultationRequestSerializer(consultation_request,data=request.data,partial=True)
             if(serializer.is_valid()):
                 serializer.save()
-                return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+                return CustomResponse(serializer.data,status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=6,data="درخواست مشاوره"))
+            return CustomResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست مشاوره با خطا مواجه شد"))
     
         def delete(self,request):
             id = request.GET.get('id')
             if(id == None):
-                return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+                return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست مشاوره"))
             consultation_request = get_object_or_404(ConsultationRequest,id=id)
             consultation_request.delete()
-            return Response("deleted",status=status.HTTP_202_ACCEPTED)
+            return CustomResponse("deleted",status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=7,data="درخواست مشاوره"))
 
 class ContactUsView(APIView):
 
@@ -96,31 +96,31 @@ class ContactUsView(APIView):
         serializer = ContactUsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse(serializer.data, status=status.HTTP_201_CREATED,message=CustomMessage(type=5,data="درخواست تماس با ما"))
+        return CustomResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست تماس با ما با خطا مواجه شد"))
     
 
 class ContactUsAdminView(APIView):
     def get(self, request):
         contact_us = ContactUs.objects.all()
         serializer = ContactUsSerializer(contact_us, many=True)
-        return Response(serializer.data)
+        return CustomResponse(serializer.data,status=status.HTTP_200_OK,message=CustomMessage(type=1,data="درخواست تماس با ما"))
     
     def put (self,request):
         if('id' not in request.data or 'id' =='' ):
-            return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست تماس با ما"))
         id=request.data["id"]
         contact_us = get_object_or_404(ContactUs,id=id)
         serializer = ContactUsSerializer(contact_us,data=request.data,partial=True)
         if(serializer.is_valid()):
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse(serializer.data,status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=6,data="درخواست تماس با ما"))
+        return CustomResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=2,data="درخواست تماس با ما با خطا مواجه شد"))
 
     def delete(self,request):
         id = request.GET.get('id')
         if(id == None):
-            return Response("neeed id",status=status.HTTP_400_BAD_REQUEST)
+            return CustomResponse("neeed id",status=status.HTTP_400_BAD_REQUEST,message=CustomMessage(type=3,data="درخواست تماس با ما"))
         contact_us = get_object_or_404(ContactUs,id=id)
         contact_us.delete()
-        return Response("deleted",status=status.HTTP_202_ACCEPTED)
+        return CustomResponse("deleted",status=status.HTTP_202_ACCEPTED,message=CustomMessage(type=7,data="درخواست تماس با ما"))
